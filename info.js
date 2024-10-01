8af19e423af2853966713e32b8d36b73dbf0d983
JenW <jenmarie431@gmail.com>
2024-10-01 16:51:32 -0500

    live
    posting updated changes, still need to work on responsive view for sizes in-between mobile and desktop.

--- info.js
+++ info.js
@@ -1,32 +1,32 @@
 // slideshow carosel for ux project
 
-let slideIndex = 1;
-showSlides(slideIndex);
+let slideIndexx = 1;
+showSlides(slideIndexx);
 
 // Next/previous controls;
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function plusSlide(n) {
+  showSlides(slideIndexx += n);
 }
 
 // Thumbnail image controls;
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+function currentSlidez(n) {
+  showSlides(slideIndexx = n);
 }
 
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  let dot = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndexx = 1}
+  if (n < 1) {slideIndexx = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+  for (i = 0; i < dot.length; i++) {
+    dot[i].className = dot[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[slideIndexx-1].style.display = "block";
+  dot[slideIndexx-1].className += " active";
 
 } 
 
@@ -54,7 +54,7 @@ function currentSlide(n) {
 function showSlidess(n) {
   var i;
   var slides = document.getElementsByClassName("gallerySlides");
-  var dots = document.getElementsByClassName("demo-cursor");
+  var dots = document.getElementsByClassName("hover-shadow cursor");
   var captionText = document.getElementById("caption");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
@@ -67,4 +67,104 @@ function showSlidess(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+
+// modal for second box
+
+
+
+function openModals() {
+  document.getElementById("myModals").style.display = "block";
+}
+
+function closeModals() {
+  document.getElementById("myModals").style.display = "none";
+}
+
+
+function plusSlidesss(n) {
+  showSlidesss(slideIndex += n);
+}
+
+function currentSlides(n) {
+  showSlidesss(slideIndex = n);
+}
+
+function showSlidesss(n) {
+  var i;
+  var slidess = document.getElementsByClassName("gallerySlidess");
+  var dots = document.getElementsByClassName("demos");
+  var captionText = document.getElementById("captions");
+  if (n > slidess.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slidess.length}
+  for (i = 0; i < slidess.length; i++) {
+      slidess[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slidess[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+  
+}
+
+
+// modal
+var minimodal = document.getElementById("smolmodal");
+// get image 
+
+var imgmini = document.getElementById("miniimg");
+var modalImg = document.getElementById("img1")
+var minicaption = document.getElementById("minicaption");
+imgmini.onclick = function () {
+  minimodal.style.display="block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// get span that closes the modal
+
+var span= document.getElementsByClassName("xclose")[0];
+
+// action to close the modal with x
+
+span.onclick = function(){
+  minimodal.style.display = "none";
+}
+
+// second mini modal
+
+
+var minimodal2 = document.getElementById("smolmodals");
+// get image 
+
+var imgmini2 = document.getElementById("miniimg2");
+var modalImgs = document.getElementById("img2")
+var minicaption2 = document.getElementById("minicaption2");
+imgmini2.onclick = function () {
+  minimodal2.style.display="block";
+  modalImgs.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// get span that closes the modal
+
+var span= document.getElementsByClassName("xclosex")[0];
+
+// action to close the modal with x
+
+span.onclick = function(){
+  minimodal2.style.display = "none";
+}
+
+
+
+
+// mobile menu
+
+
+function myFunction(x) {
+  x.classList.toggle("change");
 }
